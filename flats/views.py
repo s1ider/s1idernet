56c71e30a9a8b29ccdea51db52f8aa2d8395a6e8
s1ider (work) <s1iderorama@gmail.com>
2011-11-25 17:36:15 +0200

    add Yandex geocoder to search processing

--- flats/views.py
+++ flats/views.py
@@ -1,7 +1,9 @@
+# coding: utf-8
 from urllib import urlencode,urlopen
 from django.shortcuts import render_to_response
 from django.template.response import TemplateResponse
 from flats.models import SearchHistory
+import simplejson
 
 def search(request):
 	if request.method == 'POST':
@@ -9,11 +11,14 @@ def search(request):
 		params = urlencode({
 			'geocode' : search_query,
 			'format' : 'json',
-			'api' : 'ALkUyk4BAAAApRinSwIAS_pBYGgk9hlNbOWvjOx3Zi8VGBIAAAAAAAAAAADG1QihAV-jqGTltsWsd3ylJFXKPw=='
+			'api' : u'ALkUyk4BAAAApRinSwIAS_pBYGgk9hlNbOWvjOx3Zi8VGBIAAAAAAAAAAADG1QihAV-jqGTltsWsd3ylJFXKPw=='
 		})
-		f = urlopen('http://geocode-maps.yandex.ru/1.x/', params)
-		print f.read()
+		response_json = urlopen('http://geocode-maps.yandex.ru/1.x/', params)
+		result = simplejson.load(response_json)
+		if 'error' in result:
+			result = "Error has happened: " + result['error']['message']
 	else:
 		pass
 	return TemplateResponse(request, 'flats.html', {
-		'search_text' : search_query})
+		'search_text' : search_query,
+		'result' : result})
