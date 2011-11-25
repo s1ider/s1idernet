1c259db7b13b31273a5e0e5ae7160fb8b47d3297
s1ider (work) <s1iderorama@gmail.com>
2011-11-25 16:13:59 +0200

    looks better without border: none

--- flats/views.py
+++ flats/views.py
@@ -1,3 +1,4 @@
+from urllib import urlencode,urlopen
 from django.shortcuts import render_to_response
 from django.template.response import TemplateResponse
 from flats.models import SearchHistory
@@ -5,6 +6,13 @@ from flats.models import SearchHistory
 def search(request):
 	if request.method == 'POST':
 		search_query = request.POST['search']
+		params = urlencode({
+			'geocode' : search_query,
+			'format' : 'json',
+			'api' : 'ALkUyk4BAAAApRinSwIAS_pBYGgk9hlNbOWvjOx3Zi8VGBIAAAAAAAAAAADG1QihAV-jqGTltsWsd3ylJFXKPw=='
+		})
+		f = urlopen('http://geocode-maps.yandex.ru/1.x/', params)
+		print f.read()
 	else:
 		pass
 	return TemplateResponse(request, 'flats.html', {
