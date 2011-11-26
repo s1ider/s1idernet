db11d0c4f5e118f39f692853f9c89a94a0842915
s1ider <s1iderorama@gmail.com>
2011-11-27 00:34:37 +0200

    handle object not found.

--- flats/views.py
+++ flats/views.py
@@ -1,6 +1,5 @@
 # coding: utf-8
 from urllib import urlencode,urlopen
-from django.shortcuts import render_to_response
 from django.template.response import TemplateResponse
 from flats.models import SearchHistory
 import json
@@ -14,12 +13,14 @@ def search(request):
 			'key' : 'ALkUyk4BAAAApRinSwIAS_pBYGgk9hlNbOWvjOx3Zi8VGBIAAAAAAAAAAADG1QihAV-jqGTltsWsd3ylJFXKPw=='
 		})
 		response_json = urlopen('http://geocode-maps.yandex.ru/1.x/', params)
-		result = json.load(response_json)
-		if 'error' in result:
-			result = "Error has happened: " + result['error']['message']
-		result = result['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos']
-	else:
-		pass
+		response = json.load(response_json)
+		if 'error' in response:
+			result = "Error has happened: " + response['error']['message']
+        response = response['response']['GeoObjectCollection']['featureMember']
+        if len(response) > 0:
+		    result = response[0]['GeoObject']['Point']['pos']
+        else:
+            result = "Object '%s' was not found." % (search_query)
 	return TemplateResponse(request, 'flats.html', {
 		'search_text' : search_query,
 		'result' : result})
